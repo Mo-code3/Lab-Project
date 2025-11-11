635563de037acaac3a67f1d198c868488505c7b8
Mohammed Abdul Wahab Azazi <20240005212@students.cud.ac.ae>
2025-11-11 20:10:28 +0400

    Newest PerishableProduct (use for phase 1)

--- PerishableProduct.java
+++ PerishableProduct.java
@@ -1,19 +1,24 @@
 package LabProject.Main;
 
 public class PerishableProduct extends Product {
-    
     private String expiryDate;
 
+    public PerishableProduct(int productId, String productName, double price, int quantity, String expiryDate) {
+        super(productId, productName, price, quantity);
+        this.expiryDate = expiryDate;
+    }
+
     public String getExpiryDate() {
-        return expiryDate;
+        return expiryDate; 
     }
 
     public void setExpiryDate(String expiryDate) {
         this.expiryDate = expiryDate;
     }
+
+    // Overriding
     public void displayProductInfo() {
-        super.displayProductInfo(); // show base info
+        super.displayProductInfo();
         System.out.println("Expiry Date: " + expiryDate);
     }
 }
-
